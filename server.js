9a3f5ec8a82db5ff15b8742da0323ba463d1d9a2
Sam007-dot <shivamkumbar44@gmail.com>
2025-03-26 20:45:41 +0530

    changd npm package

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRouter = require('./routers/userRouter');
+require('dotenv').config();
+const planRoutes = require("./routers/planRoutes");
+const querryRoutes = require("./routers/querryRoutes")
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({
+    origin: 'https://tutorji.netlify.app'
+  }));  
+app.use(bodyParser.json());
+app.use('/uploads', express.static('uploads'));
+app.use('/api/users', userRouter);
+
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Db connected & Server running on port ${PORT}`)))
+    .catch(err => console.log(err));
